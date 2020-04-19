305e275607d827c4532d777700f863993e104dfc
rajdeeprockysarker <rajdeepsarker1986@gmail.com>
2020-04-19 20:21:39 +0530

    Images Added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -47,6 +47,10 @@ export class HomePage {
   mHumidity: string;
 
 
+  mCurrentWeatherIcon:string;
+  mFiveDaysWeatherIcon=[];
+
+
 
   constructor(public mRepositoryyAPIService: RepositoryService,
     public mUIServiceService: UIServiceServiceService,
@@ -89,6 +93,7 @@ export class HomePage {
         this.mWeather = (JSON.parse(mCurrentValue).weather)[0].description;
         this.mWind = (JSON.parse(mCurrentValue).wind).speed;
         this.mHumidity = (JSON.parse(mCurrentValue).main).humidity + "%";
+        this.mCurrentWeatherIcon=this.getWeatherIconFromAssetFolder(JSON.parse(mCurrentValue).weather[0].description);
 
         ///// Insert Date into this.mDate Array
         for (let i = 0; i < JSON.parse(mFiveDaysValue).list.length; i++) {
@@ -103,18 +108,21 @@ export class HomePage {
 
           var mHeigh = 0;
           var mLow = 0;
+          var mWeatherIcon="";
 
           for (let j = 0; j < JSON.parse(mFiveDaysValue).list.length; j++) {
             var mDateAfterAplit = (JSON.parse(mFiveDaysValue).list[j].dt_txt).split(" ")[0];
             if (mDateAfterAplit == this.mDate[i]) {
               if (mHeigh == 0 && mLow == 0) {
                 mHeigh = JSON.parse(mFiveDaysValue).list[j].main.temp_max;
+                mWeatherIcon=this.getWeatherIconFromAssetFolder(JSON.parse(mFiveDaysValue).list[j].weather[0].icon);
                 mLow = JSON.parse(mFiveDaysValue).list[j].main.temp_min;
                 continue;
               }
               else {
                 if (mHeigh < JSON.parse(mFiveDaysValue).list[j].main.temp_max) {
                   mHeigh = JSON.parse(mFiveDaysValue).list[j].main.temp_max;
+                  mWeatherIcon=this.getWeatherIconFromAssetFolder(JSON.parse(mFiveDaysValue).list[j].weather[0].icon);
                 }
                 if (mLow > JSON.parse(mFiveDaysValue).list[j].main.temp_min) {
                   mLow = JSON.parse(mFiveDaysValue).list[j].main.temp_min;
@@ -127,6 +135,7 @@ export class HomePage {
 
           this.mDateTempMax.push(Math.floor(this.mTemperatureConverterService.kelvinToCelcius(mHeigh)));
           this.mDateTempMin.push(Math.floor(this.mTemperatureConverterService.kelvinToCelcius(mLow)));
+          this.mFiveDaysWeatherIcon.push(mWeatherIcon);
 
           console.log(this.mTemperatureConverterService.kelvinToCelcius(mHeigh)
             + "     " + this.mTemperatureConverterService.kelvinToCelcius(mLow));
@@ -166,6 +175,96 @@ export class HomePage {
       }
     }
     this.mUIServiceService.dismissLoading();
+
+  }
+
+  public getWeatherIconFromAssetFolder(mWeatherType:string):string{
+
+    switch (mWeatherType) {
+      case "clear sky": {
+         return "../../assets/images/clear_sky.png";
+         break;
+      }
+     
+      case "few clouds": {
+        return "../../assets/images/few_clouds.png";
+         break;
+      }
+     
+      case "scattered clouds": {
+        return "../../assets/images/scattered_clouds.png";
+         break;
+      }
+     
+      case "broken clouds": {
+        return "../../assets/images/broken_clouds.png";
+         break;
+      }
+      case "shower rain": {
+        return "../../assets/images/shower_rain.png";
+        break;
+     }
+      case "rain": {
+        return "../../assets/images/rain.png";
+        break;
+     }
+      case "snow": {
+        return "../../assets/images/snow.png";
+        break;
+     }
+
+     case "mist": {
+      return "../../assets/images/mist.png";
+      break;
+   }
+   case "mist": {
+    return "../../assets/images/mist.png";
+    break;
+ }
+ 
+ case "01d": {
+  return "../../assets/images/01d.png";
+  break;
+}
+  
+case "02d": {
+  return "../../assets/images/02d.png";
+  break;
+}
+case "03d": {
+  return "../../assets/images/03d.png";
+  break;
+}
+
+case "04d": {
+  return "../../assets/images/04d.png";
+  break;
+}
+case "09d": {
+  return "../../assets/images/09d.png";
+  break;
+}
+case "10d": {
+  return "../../assets/images/10d.png";
+  break;
+}
+case "11d": {
+  return "../../assets/images/11d.png";
+  break;
+}
+case "13d": {
+  return "../../assets/images/13d.png";
+  break;
+}
+case "50d": {
+  return "../../assets/images/50d.png";
+  break;
+}
+default: {
+        return "../../assets/images/clear_sky.png";
+         break;
+      }
+   }
 
   }
 
