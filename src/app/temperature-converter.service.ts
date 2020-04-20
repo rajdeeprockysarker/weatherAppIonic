6e343afa3a8449c8f693630953c74ab638227060
rajdeeprockysarker <rajdeepsarker1986@gmail.com>
2020-04-20 17:02:12 +0530

    TemperatureConverter Service Documentation

--- src/app/temperature-converter.service.ts
+++ src/app/temperature-converter.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@angular/core';
-
+/**
+ * Inject to root of app
+*/
 @Injectable({
   providedIn: 'root'
 })
+/**
+ * Service for Temperature Converter
+*/
 export class TemperatureConverterService {
 
+  /**
+   * @ignore
+   */
   constructor() { }
 
+  /**
+   * Convert from Kelvin to Celcius
+   * @param params Kelvin temperature
+   */
   public kelvinToCelcius(params){
     return (params-273.15)
   }
